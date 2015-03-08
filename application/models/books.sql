cbc20f1de5006028a113123eb8c7eb6700f77332
rene-ye <rene.ye@hotmail.com>
2015-03-08 15:16:06 -0700

    Updated the SQL Database

--- application/models/books.sql
+++ application/models/books.sql
@@ -3,29 +3,60 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2015 at 02:14 AM
+-- Generation Time: Mar 08, 2015 at 11:15 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
- 
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
- 
- 
+
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
- 
+
 --
 -- Database: `books`
 --
- 
+
 -- --------------------------------------------------------
- 
+
 --
--- Table structure for table `book`
+-- Table structure for table `book genres`
 --
- 
+
+CREATE TABLE IF NOT EXISTS `book genres` (
+  `bookID` int(10) unsigned zerofill NOT NULL,
+  `genreID` int(8) unsigned zerofill NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `book genres`
+--
+
+INSERT INTO `book genres` (`bookID`, `genreID`) VALUES
+(0000000002, 00000002),
+(0000000002, 00000003),
+(0000000001, 00000001),
+(0000000001, 00000005),
+(0000000007, 00000002),
+(0000000007, 00000003),
+(0000000003, 00000001),
+(0000000003, 00000002),
+(0000000004, 00000001),
+(0000000004, 00000002),
+(0000000005, 00000001),
+(0000000005, 00000002),
+(0000000006, 00000001),
+(0000000006, 00000002);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
 CREATE TABLE IF NOT EXISTS `books` (
 `bookID` int(10) unsigned zerofill NOT NULL COMMENT 'A book''s ID # (00001234)',
   `title` text NOT NULL COMMENT 'A book''s title (The Green Mile)',
@@ -33,63 +64,68 @@ CREATE TABLE IF NOT EXISTS `books` (
   `date_pub` date DEFAULT NULL COMMENT 'Date of publication',
   `date_load` date NOT NULL COMMENT 'Date of upload',
   `uploader` text COMMENT 'Owner'
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
- 
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
 --
--- Dumping data for table `book`
+-- Dumping data for table `books`
 --
- 
+
 INSERT INTO `books` (`bookID`, `title`, `author`, `date_pub`, `date_load`, `uploader`) VALUES
 (0000000001, '50 Shades of Grey', 'E. L. James', '2011-03-01', '2015-03-05', 'admin'),
-(0000000002, '1984', 'George Orwell', '1949-06-08', '2015-03-05', 'admin');
- 
+(0000000002, '1984', 'George Orwell', '1949-06-08', '2015-03-05', 'admin'),
+(0000000003, 'The Fellowship of the Ring', 'J. R. R. Tolkien', '1954-06-01', '2015-03-08', 'admin'),
+(0000000004, 'The Two Towers', 'J. R. R. Tolkien', '1954-11-01', '2015-03-08', 'admin'),
+(0000000005, 'The Return of the King', 'J. R. R. Tolkien', '1955-10-01', '2015-03-08', 'admin'),
+(0000000006, 'The Hobbit', 'J. R. R. Tolkien', '1937-09-01', '2015-03-08', 'admin'),
+(0000000007, 'Lord of the Flies', 'William Golding', '1954-09-17', '2015-03-08', 'admin');
+
 -- --------------------------------------------------------
- 
+
 --
 -- Table structure for table `genres`
 --
- 
+
 CREATE TABLE IF NOT EXISTS `genres` (
 `genreID` smallint(8) unsigned zerofill NOT NULL,
   `genreName` varchar(255) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
- 
+
 --
 -- Dumping data for table `genres`
 --
- 
+
 INSERT INTO `genres` (`genreID`, `genreName`) VALUES
 (00000001, 'Action'),
 (00000002, 'Adventure'),
 (00000004, 'Comedy'),
 (00000003, 'Horror'),
 (00000005, 'Romance');
- 
+
 --
 -- Indexes for dumped tables
 --
- 
+
 --
--- Indexes for table `book`
+-- Indexes for table `books`
 --
 ALTER TABLE `books`
  ADD PRIMARY KEY (`bookID`), ADD UNIQUE KEY `bookID` (`bookID`);
- 
+
 --
 -- Indexes for table `genres`
 --
 ALTER TABLE `genres`
  ADD PRIMARY KEY (`genreID`), ADD UNIQUE KEY `genreName` (`genreName`);
- 
+
 --
 -- AUTO_INCREMENT for dumped tables
 --
- 
+
 --
--- AUTO_INCREMENT for table `book`
+-- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-MODIFY `bookID` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT COMMENT 'A book''s ID # (00001234)',AUTO_INCREMENT=3;
+MODIFY `bookID` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT COMMENT 'A book''s ID # (00001234)',AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `genres`
 --
