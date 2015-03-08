4d61cabff95ac027652391a92ef44d6fbaeb7c2e
Peter Chan <chan-p@outlook.com>
2015-03-07 22:20:53 -0800

    Publish Date format validation fixed Signed-off-by:Peter Chan <chan-p@outlook.com>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -46,18 +46,21 @@ class Admin extends Application {
                 $message .=$booboo . BR;
             }
         }
+        $this->data['pageTitle'] = 'Add Book';	
         $this->data['message'] = $message;
         $this->data['fbookID'] = makeTextField('ID#', 'bookID', $book->bookID, "Unique book "
-                . "identifier, system-assigned", 10, 10, true);
-        $this->data['ftitle'] = makeTextField('Title', 'title', $book->title);
-        $this->data['fauthor'] = makeTextField('Author', 'author', $book->author);
-        $this->data['fdate_pub'] = makeTextField('Date Published', 'date_pub', $book->date_pub);
+                . "identifier, system-assigned" . BR, 10, 10, true);
+        $this->data['ftitle'] = makeTextField('Title', 'title', $book->title, ''.BR);
+        $this->data['fauthor'] = makeTextField('Author', 'author', $book->author, ''.BR);
+        $this->data['fdate_pub'] = makeTextField('Date Published', 'date_pub', $book->date_pub, "Date format:"
+                . " YYYY/MM/DD" . BR . BR);
         $date = new DateTime(null, new DateTimeZone('America/New_York'));
+        //auto insert upload date
         $formatDate = $date->format('Y-m-d');
         $book->date_load = $formatDate;
         $this->data['fdate_load'] = makeTextField('Date Uploaded', 'date_load', $book->date_load, "Auto date insert"
-                , 10, 10, true);
-        $this->data['fuploader'] = makeTextField('Uploader', 'uploader', $book->uploader);
+                .BR .BR, 10, 10, true);
+        $this->data['fuploader'] = makeTextField('Uploader', 'uploader', $book->uploader, ''.BR);
         $this->data['pagebody'] = 'book_edit';
         $this->data['fsubmit'] = makeSubmitButton('Process Book',
                 "Click here to validate the book data", 'btn-success');
@@ -72,7 +75,9 @@ class Admin extends Application {
         $record->bookID = $this->input->post('bookID');
         $record->title = $this->input->post('title');
         $record->author = $this->input->post('author');
-        $record->date_pub = $this->input->post('date_pub');
+        //set date published to a variable to check date format later
+        $dateVal = $this->input->post('date_pub');
+        //make today's Date and set time zone and date format
         $date = new DateTime(null, new DateTimeZone('America/New_York'));
         $formatDate = $date->format('Y-m-d');
         $record->date_load = $formatDate;
@@ -82,14 +87,17 @@ class Admin extends Application {
             $this->errors[] = 'You must specify a title.';
         if(empty($record->author))
             $this->errors[] = 'You must specify an author.';
+        // date format validation
+        if(($timeVal = strtotime($dateVal)) === false)
+                $this->errors[] = 'Date format YYYY/MM/DD';
+        else
+            $record->date_pub = date('Y-m-d', $timeVal);
+        
         if(empty($record->date_pub))
-            $this->errors[] = 'You must specify a publish date.';
+            $this->errors[] = 'You must specify a publish date. YYYY/MM/DD';
         if(empty($record->uploader))
             $this->errors[] = 'You must specify an uploader.';
         
-        $dt = DateTime::createFromFormat("Y-m-d", $record->date_pub);
-        if(empty($dt))
-            $this->error[] = 'Date format is YYYY/MM/DD';
 
         // redisplay if any errors
         if(count($this->errors) > 0)
